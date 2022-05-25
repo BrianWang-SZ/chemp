76923523203803e53e6221c2353ff018c9ef7f69
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-05-25 18:02:37 -0400

    Create const.hpp

---
+++ Project#02/const.hpp
@@ -0,0 +1,12 @@
+// 1 A = ANGSTROM m
+#define ANGSTROM 1.00001495e-10
+// 1 Bohr = BOHR m
+#define BOHR 5.29177210903e-11
+// 1 amu = AMU kg
+#define AMU 1.66053906660e-27
+// speed of light = LIGHT m/s
+#define LIGHT 299792458
+// planck's constant = PLANK J/Hz
+#define PLANCK 6.62607015e-34
+// 1 Hartree = HARTREE J
+#define HARTREE 4.3597447222071e-18
