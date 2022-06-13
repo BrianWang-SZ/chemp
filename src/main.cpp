63c19723384e5268f2af39837585d05412054644
Brian Wang <wangruohe@gmail.com>
2022-06-13 18:58:06 -0400

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 #include "VibSolver.hpp"
 
 int main (int argc, char **argv){
-    const char *s = "../input/h2o_geom.txt"; 
+    const char *s = "../input"; 
     Molecule m(s);
     GeomSolver gs(m);
     gs.solve();
-    VibSolver vs(m, "../input/h2o_hessian.txt");
+    VibSolver vs(m);
     vs.solve();
 }
