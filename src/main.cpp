5a7e57aaa0d4bd5675e4054682e4875dd0e623a8
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-06-15 12:37:18 -0400

    d

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,12 @@
 int main (int argc, char **argv){
     const char *s = "../input/h2o/STO-3G"; 
     Molecule m(s);
-    HfSolver hfs(m, true);
-    hfs.compute();
+    //HfSolver hfs(m, true);
+    //hfs.compute();
     // GeomSolver gs(m);
     // gs.solve();
     // VibSolver vs(m);
     // vs.solve();
-    //CCSolver ccs(m);
-    //ccs.compute();
+    CCSolver ccs(m);
+    ccs.compute();
 }
