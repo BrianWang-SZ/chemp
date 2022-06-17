61b3c58d1f7aef5dabb156ff6e46ffaea90a7df1
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-06-16 20:20:18 -0400

    d

--- src/HfSolver.cpp
+++ src/HfSolver.cpp
@@ -112,23 +112,19 @@ double HfSolver::compute(){
             DIIS d;
             Matrix e = F * D * S - S * D * F;
             d.add(F, e);
-            Matrix F_old = F;
             F = d.extrap();
-
-            Matrix new_D(norb, norb);
-            updateDensity(new_D);
-
-            rms = Helper::calc_rms(D, new_D);
-        
-            D = new_D;
-            E_curr = calc_hf_energy();
-            delta_E = E_curr - E_prev;
-            F = F_old;
         }
         
         /* DIIS optimization ends*/
 
+        Matrix new_D(norb, norb);
+        updateDensity(new_D);
+
+        rms = Helper::calc_rms(D, new_D);
         
+        D = new_D;
+        E_curr = calc_hf_energy();
+        delta_E = E_curr - E_prev;
 
         
 
