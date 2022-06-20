d4445312302a82e68c33b123781681766de95a95
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-06-20 13:00:47 -0400

    d

--- src/CCSolver.cpp
+++ src/CCSolver.cpp
@@ -332,9 +332,9 @@ void CCSolver::updateT(Matrix Fae, Matrix Fmi, Matrix Fme,
                     // Term 1
                     tmp4d[a][b][i][j] = mospin[i][j][a][b];
                     for (int e = noso; e < nso; e++){
-                        // Term 2.1
-                        // tmp4d[a][b][i][j] += t_abij[a][e][i][j] * Fae(b, e) - 
-                        //                      t_abij[b][e][i][j] * Fae(a, e);
+                        Term 2.1
+                        tmp4d[a][b][i][j] += t_abij[a][e][i][j] * Fae(b, e) - 
+                                             t_abij[b][e][i][j] * Fae(a, e);
                         // Term 7
                         tmp4d[a][b][i][j] += t_ai[e][i] * mospin[a][b][e][j] - 
                                              t_ai[e][j] * mospin[a][b][e][i];
