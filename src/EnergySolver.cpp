471f8af0ad3ca5f18512652e37f3da66f464def9
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-06-20 17:05:13 -0400

    d

--- src/EnergySolver.cpp
+++ src/EnergySolver.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 
 #define MAXORB 100
-#define INDEX(i, j) ((i>j) ? (((i)*((i)+1)/2)+(j)) : (((j)*((j)+1)/2)+(i)))
+#define INDEX(i,j) (i>j) ? (ioff[i]+j) : (ioff[j]+i)
 
 EnergySolver::EnergySolver(Molecule &m, bool toprint){
     this -> toprint = toprint;
