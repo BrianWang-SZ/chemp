90f19bd7a9c85f92567e1a0b178f8fe235021a5e
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-06-23 17:08:14 -0400

    d

--- src/DIIS.cpp
+++ src/DIIS.cpp
@@ -56,7 +56,7 @@ Matrix DIIS::build_B(){
     Matrix B(size + 1, size + 1);
     for (int i = 0; i < size; i++){
         for (int j = 0; j <= i; j++){
-            B(i, j) = (err[i].transpose() * err[j])(0, 0);
+            B(i, j) = (err[i].transpose() * err[j]).trace();
             B(j, i) = B(i, j);  //symmetry
         }
     }
