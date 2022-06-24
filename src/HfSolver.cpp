f6ab2cb12d9b7346959388eec62ce910e99f16b1
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-06-24 16:04:24 -0400

    d

--- src/HfSolver.cpp
+++ src/HfSolver.cpp
@@ -100,13 +100,15 @@ double HfSolver::compute(){
     while (count < MAXITER && (abs(delta_E) >= DELTA_1 || rms >= DELTA_2)){
 
         E_prev = E_curr;
+
+        Matrix e = F * D * S - S * D * F;
+        d.add(F, e);
         
         Matrix new_D(norb, norb);
 
         /* DIIS optimization starts*/
         if(count >= 2){
-            Matrix e = F * D * S - S * D * F;
-            d.add(F, e);
+            
 
             Matrix Fext(norb, norb);
             d.extrap(Fext);
@@ -119,8 +121,8 @@ double HfSolver::compute(){
         } else {
             updateFock(F, D);
 
-            Matrix e = F * D * S - S * D * F;
-            d.add(F, e);
+            // Matrix e = F * D * S - S * D * F;
+            // d.add(F, e);
 
             updateDensity(new_D, F);
             
