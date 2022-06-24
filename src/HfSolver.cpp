e731a2606dfc9c3ea5509ad7b968b587f7e8ff7b
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-06-24 14:49:16 -0400

    d

--- src/HfSolver.cpp
+++ src/HfSolver.cpp
@@ -103,46 +103,44 @@ double HfSolver::compute(){
         
         Matrix new_D(norb, norb);
 
-        // /* DIIS optimization starts*/
-        // if(count >= 2){
-        //     Matrix Fext = d.extrap();
-        //     updateDensity(new_D, Fext);
+        /* DIIS optimization starts*/
+        if(count >= 2){
+            Matrix Fext = d.extrap();
+            updateDensity(new_D, Fext);
             
-        //     updateFock(F, new_D);
+            updateFock(F, new_D);
 
-        //     Matrix e = F * new_D * S - S * D * F;
+            Matrix e = F * new_D * S - S * D * F;
 
-        //     d.add(F, e);
+            d.add(F, e);
 
-        // } else {
-        //     updateDensity(new_D, F);
+        } else {
+            updateFock(F, D);
 
-        //     updateFock(F, new_D);
+            Matrix e = F * D * S - S * D * F;
+            d.add(F, e);
+
+            updateDensity(new_D, F);
             
-        //     if(toprint && count == 0){
-        //         printf("\tFock Matrix:\n\n");
-        //         Helper::print_matrix(F);
-        //     }
-
-            
-            
-        //     Matrix e = F * D * S - S * D * F;
-        //     d.add(F, e);
-        // }
+            if(toprint && count == 0){
+                printf("\tFock Matrix:\n\n");
+                Helper::print_matrix(F);
+            }
+        }
         /* DIIS optimization ends*/
 
         /****/
-        updateFock(F, D);
+        // updateFock(F, D);
 
-        Matrix e = F * D * S - S * D * F;
-        Helper::print_matrix(e);
+        // Matrix e = F * D * S - S * D * F;
+        // Helper::print_matrix(e);
             
-        if(toprint && count == 0){
-            printf("\tFock Matrix:\n\n");
-            Helper::print_matrix(F);
-        }
+        // if(toprint && count == 0){
+        //     printf("\tFock Matrix:\n\n");
+        //     Helper::print_matrix(F);
+        // }
         
-        updateDensity(new_D, F);
+        // updateDensity(new_D, F);
         /****/
 
         rms = Helper::calc_rms(D, new_D);
