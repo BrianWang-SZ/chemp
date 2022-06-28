eb207ae5af50614d6b085482ba21ec149f6f1e52
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-06-28 14:16:55 -0400

    d

--- src/DIIS.cpp
+++ src/DIIS.cpp
@@ -12,7 +12,7 @@ DIIS::DIIS(){
 }
 
 void DIIS::add(Matrix &mat, Matrix &e){
-    Helper::print_matrix(e);
+    // Helper::print_matrix(e);
     e.resize(e.rows() * e.cols(), 1);
     if (count >= MAXERR) {
         shift();
@@ -45,7 +45,7 @@ void DIIS::extrap(Matrix &ext) const{
     b[B.cols() - 1] = -1;
     Eigen::VectorXd c = B.colPivHouseholderQr().solve(b);
     
-    std::cout << c << std::endl;
+    // std::cout << c << std::endl;
 
     for (int i = 0; i < c.size() - 1; i++){
         ext += c[i] * mats[i];
